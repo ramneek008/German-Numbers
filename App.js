497b0ce1a661c45283ce7f0a3cbec2137b047cf3
ramneek008 <singhramneek274@gmail.com>
2021-01-13 19:16:08 +0530

    app layout

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity
+} from 'react-native';
+
+
+const soundList = [
+  require('./assets/one.mp3'),
+  require('./assets/two.mp3'),
+  require('./assets/three.mp3'),
+  require('./assets/four.mp3'),
+  require('./assets/five.mp3'),
+  require('./assets/six.mp3'),
+  require('./assets/seven.mp3'),
+  require('./assets/eight.mp3'),
+  require('./assets/nine.mp3'),
+  require('./assets/ten.mp3')
+];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <StatusBar />
+      <Image style={styles.logo} source={require('./assets/germany.png')} />
+      <View>
+        {soundList.map((sound) => (
+            <TouchableOpacity>
+              <Text>Press</Text>
+            </TouchableOpacity>
+          ))}
+      </View>
+    </ScrollView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor: "#3d0099"
   },
-});
+  logo: {
+    alignSelf: "center",
+    width:140,
+    height:70,
+    marginTop:10
+  }
+})
