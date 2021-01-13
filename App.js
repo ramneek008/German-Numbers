fbc6dd3901909a84189334690baff567bdb3b0b1
ramneek008 <singhramneek274@gmail.com>
2021-01-13 20:39:13 +0530

    number-buttons

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+// import Sound from 'react-native-sound';
 
 const soundList = [
   require('./assets/one.mp3'),
@@ -29,10 +30,15 @@ const App = () => {
     <ScrollView style={styles.container}>
       <StatusBar />
       <Image style={styles.logo} source={require('./assets/germany.png')} />
-      <View>
-        {soundList.map((sound) => (
-            <TouchableOpacity>
-              <Text>Press</Text>
+      <Text style={styles.heading}>Numbers</Text>
+      <Text style={styles.subheading}>(Die Zahlen)</Text>
+      <View style={styles.gridContainer}>
+        {soundList.map((sound, index) => (
+            <TouchableOpacity 
+              key={sound}
+              style={styles.box}
+              >
+              <Text style={styles.text}>{index+1}</Text>
             </TouchableOpacity>
           ))}
       </View>
@@ -45,12 +51,45 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor: "#3d0099"
+    backgroundColor: "#27373f"
   },
   logo: {
     alignSelf: "center",
-    width:140,
-    height:70,
+    width:170,
+    height:85,
     marginTop:10
+  },
+  heading: {
+    fontSize:35,
+    color:"#8ec9f0",
+    alignSelf: "center",
+    marginTop:10
+  },
+  subheading: {
+    fontSize:20,
+    color:"#bbdff6",
+    alignSelf: "center",
+    marginBottom:10
+  },
+  gridContainer:{
+    flex:1,
+    margin: 5,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+    justifyContent: "space-around"
+  },
+  box: {
+    height: 90,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "46%",
+    marginVertical: 6,
+    backgroundColor: "#125887",
+    borderRadius: 5
+  },
+  text: {
+    fontSize: 40,
+    color: "#ff4301"
   }
 })
